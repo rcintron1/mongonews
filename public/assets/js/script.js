7977942e5ac106b352374466b1bececac292002a
rcintron1 <rolando.cintron@gmail.com>
2017-12-23 18:14:26 -0500

    working on button functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,30 @@
-$(document).ready(function() {
-    $(window).resize(function() {
-        var bodyheight = $(this).height()-300;;
+$(document).ready(function () {
+    $(window).resize(function () {
+        var bodyheight = $(this).height() - 300;;
         $("#articles").height(bodyheight);
     }).resize();
-});
+    $("button").on("click", function () {
+        event.preventDefault();
+        console.log(this.id);
+        switch (this.id) {
+            case "loadHome":
+                console.log("loadHome");
+                window.location.href = "/";
+                break;
+            default:
+                console.log("unknown button");
+
+        };
+    });
+})
+
+
+function filterTable(callback) {
+    var filterValue = $("#inputsearch").val();
+    console.log(filterValue);
+    if (filterValue) {
+        $('td:first-child').parent('tr:not(:contains("' + filterValue + '"))').hide();
+    } else {
+        $('tr').show();
+    }
+}
