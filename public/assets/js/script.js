9465105134c03937271215b305f68167f5e414df
Rolando Cintron <rcintron@ndi.org>
2017-12-26 12:26:47 -0500

    notes now functioning as expected. Last message on top.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,8 +6,10 @@ $(document).ready(function () {
     }).resize();
     $("body").on("click", "button" ,function () {
         event.preventDefault();
-
-        switch (this.id) {
+        var btnId = this.id;
+        var articleId =$(this).attr("data-id");
+        console.log("onclick", articleId);
+        switch (btnId) {
             case "btnHome":
                 console.log("btnHome");
                 window.location.href = "/";
@@ -27,18 +29,14 @@ $(document).ready(function () {
                 var note = {};
                 note.title = $("#noteTitle").val();
                 note.notes = $("#noteData").val();
-                
                 $.ajax({
                     method: "POST",
                     url: "api/note/" + $(this).attr("data-id"),
                     data: note
                 })
                 .done((data) => {
-                    console.log(data);
-                    var newNote = $("<div>")
-                    newNote.append("<p><strong>"+data.title+"</p></strong>");
-                    newNote.append("<p>"+data.notes+"</p>");
-                    $("#notes").append()
+                    $(".collapse").collapse('hide');
+                    addNotesToDiv(data);
                 })
                 break;
             case "delArticle":
@@ -77,26 +75,28 @@ function filterTable(callback) {
 
 function showNotes(id){
     console.log("showNotes", id);
-    var form = $('<form>');
+    var form = $('<form id="notesform">');
     
     form.append('<div class="panel panel-info"><div class="panel-heading"><h3 class="panel-title"><a data-toggle="collapse" href="#collapse1">Click here to Add new note</a></h3></div><div class="panel-collapse collapse" id="collapse1"><div class="panel-body"><div class="form-group"><label for="noteTitle">Title</label><textarea class="form-control" id="noteTitle" rows="1"></textarea></div><div class="form-group"><label for="noteData">Content</label><textarea class="form-control" id="noteData" rows="3"></textarea></div><button type="button" class="btn btn-default" id="addNote" data-id="' +id+'">Submit</button>')
 
-    $("#notes").html(form);
+    $("#formdiv").html(form);
     $.ajax({
         method: "GET",
         url: "/api/note/",
         data: {_id:id}
         })
     .done((data)=>{
-        var notes = $("<div>")
         data.map((note)=>{
-            console.log(note);
-            notes.append("<p><strong>"+note.title+"</strong></p><p>"+note.notes +"</p")
+            addNotesToDiv(note)
         });
-        $("#notes").append(notes);
     })
 }
 
+function addNotesToDiv(note){
+    var notes = $("<div>")
+    notes.prepend("<p><strong>"+note.title+"</strong></p><p>"+note.notes +"</p")
+    $("#notesdiv").prepend(notes);
+}
         // $.ajax({
         //         method: "GET",
         //         url: "api/article/" + $(this).attr("data-id")
